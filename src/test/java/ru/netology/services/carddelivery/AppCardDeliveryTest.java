92201e994345e9539aaaf526f26d7c16f3e7731c
Adamizgoi <adamizgoi97@gmail.com>
2022-10-11 02:35:19 +0300

    Task 2 tests added

--- src/test/java/ru/netology/services/carddelivery/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/services/carddelivery/AppCardDeliveryTest.java
@@ -1,7 +1,10 @@
 package ru.netology.services.carddelivery;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -9,6 +12,7 @@ import org.openqa.selenium.Keys;
 
 import java.time.LocalDate;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 import static java.time.Duration.ofSeconds;
@@ -18,7 +22,7 @@ public class AppCardDeliveryTest {
 
     @BeforeEach
     void setUp() {
-        Configuration.headless=true;
+        Configuration.headless = true;
         open("http://localhost:9999/");
         $(By.cssSelector("[data-test-id = 'date'] input")).sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME));
         $(By.cssSelector("[data-test-id = 'date'] input")).sendKeys(Keys.DELETE);
@@ -33,8 +37,12 @@ public class AppCardDeliveryTest {
         return LocalDate.now().plusDays(days).format(ofPattern("dd.MM.yyyy"));
     }
 
-    /** Проверяет, что форма отправляется и показывает правильный текст после отправки, если все поля заполнены правильно (согласно требования)
-     *
+    /** Задача №1
+     */
+
+    /**
+     * Проверяет, что форма отправляется и показывает правильный текст после отправки, если все поля заполнены правильно (согласно требованиям)
+     * <p>
      * Требования:
      * Город — один из административных центров субъектов РФ.
      * Дата — не ранее трёх дней с текущей даты.
@@ -52,5 +60,41 @@ public class AppCardDeliveryTest {
         $("[data-test-id = 'agreement'] span").click();
         $("[type='button'].button").click();
         $("[data-test-id = 'notification'] .notification__content").shouldHave(text("Встреча успешно забронирована на " + generateDate(3)), ofSeconds(15));
+    }
+
+    /** Задача №2 Условие: "Ввод двух букв в поле «Город», после чего выбор нужного города из выпадающего списка"
+     */
+
+    /**
+     * При вводе двух первых букв города в списке рекомендаций на любом месте содержится нужный город
+     */
+
+    ElementsCollection cities;
+
+    @Test
+    void shouldBeOpportunityToChooseCityFromListOfSuggestions() {
+        $("[data-test-id = 'city'] input").setValue("Ма");
+        cities = $$(".popup_visible .menu-item span");
+
+        for (SelenideElement city : cities) {
+            if (city.toString().equals("Майкоп")) {
+                city.shouldHave(exactText("Майкоп"));
+            }
+        }
+    }
+
+    /**
+     * Задача №2 Условие: "Выбор даты на неделю вперёд, начиная от текущей даты, через инструмент календаря:"
+     */
+
+    @Test
+    void shouldChooseDayNeededUsingCalendar() {
+        String day = String.valueOf(LocalDate.now().plusDays(7).getDayOfMonth());
+        $("[data-test-id='date'] button").click();
+        $(byText(day)).click();
+
+        String expected = "18.10.2022";
+        String actual = $("[data-test-id = 'date'] input").getValue();
+        Assertions.assertEquals(expected, actual);
     }
 }
