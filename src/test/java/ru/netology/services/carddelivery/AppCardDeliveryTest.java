c25e5ed679f21e216200da79182b2ea2028e15c1
Adamizgoi <adamizgoi97@gmail.com>
2022-10-12 19:40:11 +0300

    Improve tast 2 test

--- src/test/java/ru/netology/services/carddelivery/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/services/carddelivery/AppCardDeliveryTest.java
@@ -2,9 +2,7 @@ package ru.netology.services.carddelivery;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -15,6 +13,7 @@ import java.time.LocalDate;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
+import static java.lang.String.valueOf;
 import static java.time.Duration.ofSeconds;
 import static java.time.format.DateTimeFormatter.ofPattern;
 
@@ -63,38 +62,41 @@ public class AppCardDeliveryTest {
     }
 
     /** Задача №2 Условие: "Ввод двух букв в поле «Город», после чего выбор нужного города из выпадающего списка"
+     *  Условие: "Выбор даты на неделю вперёд, начиная от текущей даты, через инструмент календаря:"
      */
 
     /**
      * При вводе двух первых букв города в списке рекомендаций на любом месте содержится нужный город
      */
 
+    // коллекция для хранения всех городов из выпадающего списка городов
     ElementsCollection cities;
+
+    // дает месяц из нужной даты
+    String monthOfDate(int days) {
+        return valueOf(LocalDate.now().plusDays(days).getMonth());
+    }
+
+    // дает день из нужной даты
+    String dayOfDate(int days) {
+        return valueOf(LocalDate.now().plusDays(days).getDayOfMonth());
+    }
 
     @Test
     void shouldBeOpportunityToChooseCityFromListOfSuggestions() {
         $("[data-test-id = 'city'] input").setValue("Ма");
         cities = $$(".popup_visible .menu-item span");
-
-        for (SelenideElement city : cities) {
-            if (city.toString().equals("Майкоп")) {
-                city.shouldHave(exactText("Майкоп"));
-            }
-        }
-    }
-
-    /**
-     * Задача №2 Условие: "Выбор даты на неделю вперёд, начиная от текущей даты, через инструмент календаря:"
-     */
-
-    @Test
-    void shouldChooseDayNeededUsingCalendar() {
-        String day = String.valueOf(LocalDate.now().plusDays(7).getDayOfMonth());
+        cities.findBy(text("Майкоп")).click();
         $("[data-test-id='date'] button").click();
-        $(byText(day)).click();
-
-        String expected = generateDate(7);
-        String actual = $("[data-test-id = 'date'] input").getValue();
-        Assertions.assertEquals(expected, actual);
+        if (!monthOfDate(7).equals(monthOfDate(0))) {
+            $(".calendar__arrow[data-step='1']").click();
+        }
+        $(byText(dayOfDate(7))).click();
+        $("[data-test-id = 'name'] input").setValue("Федорова Светлана");
+        $("[data-test-id = 'phone'] input").setValue("+79060483535");
+        $("[data-test-id = 'agreement'] span").click();
+        $("[type='button'].button").click();
+        $("[data-test-id = 'notification'] .notification__content").shouldHave(text("Встреча " +
+                "успешно забронирована на " + generateDate(7)), ofSeconds(15));
     }
 }
